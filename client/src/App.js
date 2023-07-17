b62910b9cdaff7183149e04220a2431967f09042
Ashish-Abhiwan <ashish.abhiwan@gmail.com>
2023-07-17 15:55:29 +0530

    remove dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import PaymentSuccess from "./PaymentSuccess";
-import { ThemeProvider } from "@emotion/react";
-import { CSSReset, ColorModeProvider, theme } from "@chakra-ui/react";
-import ToogleBtn from "./ToogleBtn";
+// import { ThemeProvider } from "@emotion/react";
+// import { CSSReset, ColorModeProvider, theme } from "@chakra-ui/react";
+// import ToogleBtn from "./ToogleBtn";
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <ColorModeProvider>
-        <CSSReset />
-        <ToogleBtn />
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/paymentsuccess" element={<PaymentSuccess />} />
-          </Routes>
-        </Router>
-      </ColorModeProvider>
-    </ThemeProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/paymentsuccess" element={<PaymentSuccess />} />
+      </Routes>
+    </Router>
   );
 };
 
